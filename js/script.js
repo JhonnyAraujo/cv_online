36c06ba63aa2659042a644c8da0e32ff840c2f1e
Jhonny Araújo <jhonny.a.santana2002@gmail.com>
2025-09-01 16:29:52 -0300

    adicionando comentarios ao código

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Adicionando um evendo simples que caso o reconheça que o scroll é maior que 0 mude a as cores do header via css.
 window.addEventListener("scroll", function () {
   const header = document.querySelector(".header-bg");
   if (window.scrollY > 0) {
